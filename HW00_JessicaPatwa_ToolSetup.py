8f76eebc824eeb9dfd63c1fd7c6a653f5e874dbe
jessicapatwa <52906778+jessicapatwa@users.noreply.github.com>
2023-01-25 11:03:09 -0500

    Update HW00_JessicaPatwa_ToolSetup.py

--- HW00_JessicaPatwa_ToolSetup.py
+++ HW00_JessicaPatwa_ToolSetup.py
@@ -3,7 +3,7 @@
 #Class: 2023S SSW567-A
 
 import socket                                                                   # This is for importing socket to get the user name or system name
-from dateandtime import dateandtime                                             # This is for importing dateandtime to get the present Date and Time of the system
+from datetime import datetime                                             # This is for importing dateandtime to get the present Date and Time of the system
 
 def my_brand(assignment_name):                                                  # This is defining the my_brand() function to accept assignment name as an argument
     result = "=*=*=*= " + socket.getusername() + " =*=*=*=", \
@@ -17,6 +17,6 @@ def print_hello():
 
 homework_assignment_name = input("Enter Homework Assignment Name: ")            # To input the Homework Assignment Name
 
-present_date_time = dateandtime.now().strftime ("%m-%d-%y %H:%M:%S")            # To get the present Date and time from dateandtime
+present_date_time = datetime.now().strftime ("%m-%d-%y %H:%M:%S")            # To get the present Date and time from dateandtime
 print (my_brand(homework_assignment_name))                                      # This is calling my_brand() function and printing the output to console 
 print_hello()                                                                   # This is calling print_hello() function
