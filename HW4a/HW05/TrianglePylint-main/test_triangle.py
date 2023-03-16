72c260e99c6f776933be03953295325f03d14ed0
Jessica Patwa <52906778+jessicapatwa@users.noreply.github.com>
2023-03-16 03:44:40 -0400

    Add files via upload

---
+++ HW4a/HW05/TrianglePylint-main/test_triangle.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Author: Jessica Patwa
+This program is used to test the classify_triangle program's
+function used for determining the type of triangle.
+
+"""
+
+import unittest
+
+from triangle_classification import classify_triangle
+
+# This code implements the unit test functionality
+# https://docs.python.org/3/library/unittest.html has a nice description of the framework
+
+class TestTriangles(unittest.TestCase):
+    """
+        This class is used to define multiple sets of tests as
+        functions for the classify_triangle method.
+    """
+
+    def test_right_triangle_a(self):
+        """
+            To test for right angled triangle
+        """
+        self.assertEqual(classify_triangle(3,4,5),'Right angled triangle','It is a Right triangle')
+
+    def test_right_triangle_b(self):
+        """
+            To test for right angled triangle
+        """
+        self.assertEqual(classify_triangle(5,3,4),'Right angled triangle','It is a Right triangle')
+
+    def test_equilateral_triangles(self):
+        """
+            To test for equilateral triangle
+        """
+        self.assertEqual(classify_triangle(1,1,1),'Equilateral triangle','it should be equilateral')
+
+    def test_not_a_triangle(self):
+        """
+            To test if it is a triangle or not
+        """
+        self.assertEqual(classify_triangle(10,4,3),'Not a triangle','10,4,3 is not a triangle')
+
+    def test_scalene_triangles(self):
+        """
+            To test for scalene triangle
+        """
+        self.assertEqual(classify_triangle(7,4,5),'Scalene triangle','7,4,5 is a scalene triangle')
+
+    def test_isosceles_triangles(self):
+        """
+            To test for isosceles triangle
+        """
+        self.assertEqual(classify_triangle(2,3,2),'Isosceles triangle','Its an Isosceles triangle')
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
---
+++ HW4a/HW05/TrianglePylint-main/triangle_classification.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Jessica Patwa
+
+This program is used to write a function called classify_triangle() that takes
+three  parameters : a, b, and c.
+The three parameters represent the lengths of the sides of a triangle.
+The function returns a string that specifies whether the triangle is scalene, isosceles,
+equilateral, right triangle and if it cannot be a triangle then it returns 'Not a triangle'.
+
+"""
+
+print('Please enter the values for a, b and c: ')
+x=int(input())
+y=int(input())
+z=int(input())
+
+
+def classify_triangle(a_s,b_s,c_s):
+    """
+        Function to classify triangles
+    """
+    if a_s+b_s>c_s and b_s+c_s>a_s and c_s+a_s>b_s:
+        if a_s==b_s and b_s==c_s:
+            return 'Equilateral triangle'
+        if a_s==b_s or b_s==c_s or a_s==c_s:
+            return 'Isosceles triangle'
+        if a_s**2+b_s**2==c_s**2 or b_s**2+c_s**2==a_s**2 or c_s**2+a_s**2==b_s**2:
+            return 'Right angled triangle'
+        return 'Scalene triangle'
+    return 'Not a triangle'
+
+classify_triangle(x,y,z)
